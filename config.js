662565c4dc5a0ceccea3857c57f25c88c1ddf976
Grant <a-type@outlook.com>
2015-01-16 21:33:02 -0500

    Undo that

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'https://www.grantforrest.net',
+        url: 'http://www.grantforrest.net',
         mail: {},
         database: {
             client: 'postgres',
